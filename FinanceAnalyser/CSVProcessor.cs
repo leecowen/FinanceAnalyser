274d45d5819f9cbdbc50db952424691e0bbda37e
Lee <lee.cowen@googlemail.com>
2016-12-16 17:22:38 +0000

    Improved readability of CSVProcessor

--- FinanceAnalyser/CSVProcessor.cs
+++ FinanceAnalyser/CSVProcessor.cs
@@ -33,23 +33,8 @@ namespace FinanceAnalyser
                     decimal debitFormatted;
                     decimal creditFormatted;
 
-                    if (!debit.HasValue)
-                    {
-                        debitFormatted = 0;
-                    }
-                    else
-                    {
-                        debitFormatted = debit.Value;
-                    }
-
-                    if (!credit.HasValue)
-                    {
-                        creditFormatted = 0;
-                    }
-                    else
-                    {
-                        creditFormatted = credit.Value;
-                    }
+                    if (!debit.HasValue) { debitFormatted = 0; } else { debitFormatted = debit.Value; }
+                    if (!credit.HasValue) { creditFormatted = 0; } else { creditFormatted = credit.Value; }
 
                     //Build a Transaction from the row data
                     transactions.Add(new Transaction(date, type, description, debitFormatted, creditFormatted, balance));
@@ -58,73 +43,5 @@ namespace FinanceAnalyser
                 return transactions;
             }
         }
-
-        /// <summary>
-        /// Old method using manual file reading. Prone to errors.
-        /// </summary>
-        /// <param name="filepath"></param>
-        /// <returns></returns>
-        //public static IEnumerable<Transaction> GetCSVTransactions(string filepath)
-        //{
-        //    // We change file extension here to make sure it's a .csv file.
-        //    // TODO: Error checking.
-        //    string[] lines = File.ReadAllLines(System.IO.Path.ChangeExtension(filepath, ".csv"));
-
-        //    // lines.Select allows me to project each line as a Transaction. 
-        //    // This will give me an IEnumerable<Person> back.
-        //    return lines.Select(line =>
-        //    {
-        //        string[] data = line.Split(',');
-
-        //        // We return a transaction with the data in order.
-        //        var date = data[0];
-        //        var type = data[1];
-        //        var sort = data[2];
-        //        var accnum = data[3];
-        //        var description = data[4];
-        //        var debit = data[5];
-        //        var credit = data[6];
-        //        var balance = data[7];
-
-        //        //Convert results to correct type
-        //        DateTime dateFormatted = Convert.ToDateTime(date);
-        //        string typeFormatted = Convert.ToString(type);
-        //        string descriptionFormatted = Convert.ToString(description);
-        //        decimal debitFormatted;
-        //        if (debit != "")
-        //        {
-        //            debitFormatted = Convert.ToDecimal(debit);
-        //        }
-        //        else
-        //        {
-        //            debitFormatted = 0;
-        //        }
-
-        //        decimal creditFormatted;
-        //        if (credit != "")
-        //        {
-        //            creditFormatted = Convert.ToDecimal(credit);
-        //        }
-        //        else
-        //        {
-        //            creditFormatted = 0;
-        //        }
-
-        //        decimal balanceFormatted;
-        //        if (balance != "")
-        //        {
-        //            balanceFormatted = Convert.ToDecimal(balance);
-        //        }
-        //        else
-        //        {
-        //            balanceFormatted = 0;
-        //        }
-
-        //        Transaction currentTransaction = new Transaction(dateFormatted, typeFormatted, descriptionFormatted, debitFormatted, creditFormatted, balanceFormatted);
-
-        //        //Return formatted transaction
-        //        return currentTransaction;
-        //    });
-        //}
     }
 }
